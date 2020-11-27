129d13fc1cda2d7373c6a12ef76488b39929b5a7
Waqas Ehmed <waqasehmed1@gmail.com>
2020-11-27 22:30:41 +0500

    Task # 4

--- App.js
+++ App.js
@@ -1,35 +1,38 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
 
 export default function App() {
   const [price, setPrice] = useState('')
   const [discount, setDiscount]  = useState('')
   return (
     <View style={styles.container}>
-          <TextInput
-            placeholder="Original Price"
-            style={{borderColor:'grey',borderWidth:1,width:'80%',height:40,marginBottom:10}}
-            onChangeText={(price)=>{setPrice(price)}}
-            keyboardType="decimal-pad"
-            value={price}
-          />
-           <TextInput
-            placeholder="Discount Percentage"
-            style={{borderColor:'grey',borderWidth:1,width:'80%',height:40}}
-            onChangeText={(discount)=>{setDiscount(discount)}}
-            keyboardType="decimal-pad"
-            value={discount}
-            maxLength={2}
-          />
-          {price && discount
-          ?
-              <>
-                <Text>Final Price:{price - price*(discount/100)}</Text>
-                <Text>You saved:{price*(discount/100)}</Text>
-              </>
-          :
-            null
-          }
+          <View style={styles.wrapper}>
+          <Text style={styles.header}>Discount Calculator</Text>
+              <TextInput
+                placeholder="Original Price"
+                onChangeText={(price)=>{setPrice(price)}}
+                keyboardType="decimal-pad"
+                value={price}
+                style={styles.textInput}
+              />
+              <TextInput
+                placeholder="Discount Percentage"
+                onChangeText={(discount)=>{setDiscount(discount)}}
+                keyboardType="decimal-pad"
+                value={discount}
+                maxLength={2}
+                style={styles.textInput}
+              />
+              {price && discount
+              ?
+                  <>
+                    <Text style={styles.details}>Final Price:{price - price*(discount/100)}</Text>
+                    <Text style={styles.details}>You saved:{price*(discount/100)}</Text>
+                  </>
+              :
+                null
+              }
+              </View>
     </View>
   );
 }
@@ -37,8 +40,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal:'5%',
+    backgroundColor:'#05C7F2'
   },
+  wrapper:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'white',width:'100%',height:Dimensions.get('screen').height/2.5,
+    paddingHorizontal:'5%',
+    borderRadius:20
+  },
+  textInput:{
+    borderColor:'grey',
+    borderWidth:1,
+    width:'100%',
+    height:40,
+    paddingHorizontal:20,
+    marginVertical:5,
+    borderRadius:5,
+    fontSize:18
+  },
+  details:{
+    marginVertical:5,
+    fontSize:16
+  },
+  header:{
+    fontSize:20,
+    color:'black',
+    fontWeight:'bold',
+    marginVertical:20,
+  }
 });
