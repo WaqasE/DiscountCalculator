6f6574d5e516eb2b458c9f231b83bb3cac7471b8
Waqas Ehmed <waqasehmed1@gmail.com>
2020-11-27 22:13:36 +0500

    Task # 3

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [price, setPrice] = useState('')
+  const [discount, setDiscount]  = useState('')
   return (
     <View style={styles.container}>
           <TextInput
             placeholder="Original Price"
             style={{borderColor:'grey',borderWidth:1,width:'80%',height:40,marginBottom:10}}
+            onChangeText={(price)=>{setPrice(price)}}
+            keyboardType="decimal-pad"
+            value={price}
           />
            <TextInput
             placeholder="Discount Percentage"
             style={{borderColor:'grey',borderWidth:1,width:'80%',height:40}}
+            onChangeText={(discount)=>{setDiscount(discount)}}
+            keyboardType="decimal-pad"
+            value={discount}
+            maxLength={2}
           />
+          {price && discount
+          ?
+              <>
+                <Text>Final Price:{price - price*(discount/100)}</Text>
+                <Text>You saved:{price*(discount/100)}</Text>
+              </>
+          :
+            null
+          }
     </View>
   );
 }
